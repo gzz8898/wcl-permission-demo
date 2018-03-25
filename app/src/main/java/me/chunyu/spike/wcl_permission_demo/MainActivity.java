5b9347907a388f30b3efdc6fbb15294118be87f7
gzz8898 <664131321a>
2018-03-25 22:18:57 +0800

    gradle-3.1

--- app/src/main/java/me/chunyu/spike/wcl_permission_demo/MainActivity.java
+++ app/src/main/java/me/chunyu/spike/wcl_permission_demo/MainActivity.java
@@ -7,8 +7,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.MODIFY_AUDIO_SETTINGS
     };
 
-    @Bind(R.id.main_t_toolbar) Toolbar mTToolbar;
 
     private PermissionsChecker mPermissionsChecker; // 权限检测器
 
@@ -28,9 +26,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
-
-        setSupportActionBar(mTToolbar);
 
         mPermissionsChecker = new PermissionsChecker(this);
     }
